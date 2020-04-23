4fa5508396f115abcc24854a7a560a26663e568c
Fatnaj <64014916+Fatnaj@users.noreply.github.com>
2020-04-23 03:10:49 +0200

    Base du jeu

---
+++ Base 1.php
@@ -0,0 +1,25 @@
+<!--Inclusion du fichier qui contient le code pour la connexion (Peut aussi se mettre ici)-->
+<?php
+include("ConnexionBase.php")
+ ?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Projet S2 - Base </title>
+  </head>
+  <body>
+    <!--DEBUT DU CODE PHP-->
+        <?php
+        $mysqli = connectMaBase(); //Apl de la fonction pour se connecter
+
+
+
+
+
+
+        $mysqli -> close()
+
+
+  </body>
+</html>

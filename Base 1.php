9bb706d66e7f82e5d12a7d1a6271eab87dc362ad
Fatnaj <64014916+Fatnaj@users.noreply.github.com>
2020-04-23 04:24:54 +0200

    Base enregistrement pseudo

--- Base 1.php
+++ Base 1.php
@@ -9,17 +9,73 @@ include("ConnexionBase.php")
     <title>Projet S2 - Base </title>
   </head>
   <body>
+    <!--entrer un pseudo-->
+    <script type="text/JavaScript">
+function myfunction()
+{
+      	var
+	Pseudo = prompt("Choisissez un pseudo entre 1 et 25 charactère ?","Indiquez le ici");
+  location.href = 'Base 1.php'
+}
+      alert("Bonjour " + Pseudo + ". Prêt à jouer ?");
+   </script>
+
+
+
     <!--DEBUT DU CODE PHP-->
         <?php
+
+        // Déclaration des variables pour la connexion
+  			$serveur = 'localhost';
+  			$nom_base = 'guesscorrelation';
+  			$login = 'root';		//utilisateur par défaut
+  			$motdepasse = '';		//mot de passe par défaut pour "root" (pas de pwd)
+
+        // connexion à MySQL
         $mysqli = connectMaBase(); //Apl de la fonction pour se connecter
 
+        // Check connection
+        if ($mysqli->connect_error) {
+            die("Connection failed: ". $mysqli->connect_error);
+        }
+        else {
+            echo "serveur ".$mysqli->host_info." versions ".$mysqli->server_info."<br/>";
+        }
+
+        // REQUETE 1: Création de la requête recupération des pseudo
+        //////////////////////////////////ENREGISTREMENT EN BDD//////////////////////////////////
+
+                if (isset ($_POST['valider'])){
+                    // On récupère les valeurs entrées par l'internaute :
+                    $Pseudo=$_POST['pseudo'];
+                    $Score=$_POST['0'];
+
+                    //On prépare la commande sql d'insertion
+                    $req = 'INSERT INTO pseudo ('Pseudo', 'Score') VALUES ("'.$Pseudo'", "'.$Score'")';
+                    /* on lance la commande (mysql_query) et on rédige un petit message d'erreur
+        			      pour le cas où la requête ne passe pas (or die + mysql_error())
+                    (Message qui intégrera les causes d'erreur sql)*/
+                    $mysqli->query($req) or die ('Erreur SQL ! <br>'.$req.'<br />'.$mysqli->error);
+
+        ////////////////AFFICHAGE DES PSEUDO ET SCORES  SUR LA PAGE WEB//////////////////
 
 
+        				// Création de la requête
+        				$req = "SELECT * FROM pseudo";
+        				// Envoi de la requête
+        				$result = $mysqli->query($req) or die('Erreur SQL ! <br>'.$req.'<br>'.$mysqli->error);;
+
+        				// Parcourir le tableau des enregistremente et Affichage de chaque message
+        				while($enreg = $result->fetch_array(MYSQLI_BOTH))
+        					{
+        					echo $enreg['pseudo']." ";
+        					echo $enreg['score']."<br />";
+        					}
 
 
+  
 
         $mysqli -> close()
-
-
-  </body>
+        ?>
+    </body>
 </html>
